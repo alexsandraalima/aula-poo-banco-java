332ef88c5bb5a77d1a1c20a10b23df9aedebe761
alexsandraalima <114696434+alexsandraalima@users.noreply.github.com>
2022-12-06 21:20:36 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,21 @@
+
+public class Pessoa {
+	
+	protected String nome;
+	
+	
+	public Pessoa(String nome) {
+		this.nome = nome;	
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	public String toString() {
+		return "\n-------------------------\n\n"+nome;
+	}
+		
+}
